e9f065ccd7ba4d48e770198cffe7f07c2c26657e
Leonardo Losoviz <leo@getpop.org>
2020-06-18 09:40:56 +0800

    Moved field "endpoint"

---
+++ src/FieldResolvers/CustomPostFieldResolver.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\EverythingElse\FieldResolvers;
+
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
+use PoP\CustomPosts\FieldInterfaces\CustomPostFieldInterfaceResolver;
+use PoP\API\APIUtils;
+
+class CustomPostFieldResolver extends AbstractDBDataFieldResolver
+{
+    public static function getClassesToAttachTo(): array
+    {
+        return [
+            CustomPostFieldInterfaceResolver::class,
+        ];
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return [
+            'endpoint',
+        ];
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+            'endpoint' => SchemaDefinition::TYPE_URL,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+            'endpoint' => $translationAPI->__('Endpoint to fetch the post\'s data', 'pop-posts'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        switch ($fieldName) {
+            case 'endpoint':
+                return APIUtils::getEndpoint($typeResolver->resolveValue($resultItem, 'url', $variables, $expressions, $options));
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+}
