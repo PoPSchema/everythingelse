48a62c70b15648e2b2e3d26ed042d0a874e22a92
Leonardo Losoviz <leo@getpop.org>
2020-07-16 19:39:11 +0800

    Moved field "commentsURL" to EverythingElse

---
+++ src/FieldResolvers/CustomPostFieldResolver.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoP\Comments\FieldResolvers;
+
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\ComponentModel\FieldResolvers\AbstractDBDataFieldResolver;
+use PoP\CustomPosts\FieldInterfaces\CustomPostFieldInterfaceResolver;
+
+class CustomPostFieldResolver extends AbstractDBDataFieldResolver
+{
+    public static function getClassesToAttachTo(): array
+    {
+        return [
+            CustomPostFieldInterfaceResolver::class,
+        ];
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return [
+            'commentsURL',
+        ];
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+            'commentsURL' => SchemaDefinition::TYPE_URL,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+            'commentsURL' => $translationAPI->__('URL of the comments section in the post page', 'pop-comments'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        $cmscommentsapi = \PoP\Comments\FunctionAPIFactory::getInstance();
+        $post = $resultItem;
+        switch ($fieldName) {
+            case 'commentsURL':
+                // @todo: fix this, add the ?#comment_id=... to the post URL
+                return $typeResolver->resolveValue($post, 'url', $variables, $expressions, $options);
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+}
